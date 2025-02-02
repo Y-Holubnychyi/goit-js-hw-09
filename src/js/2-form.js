9ef7ed351029bd60c445cadfdbdea8dda9b73bd0
Eugene <y.holubnychyi@gmail.com>
2025-02-02 17:08:01 -0330

    upd

---
+++ src/js/2-form.js
@@ -0,0 +1,39 @@
+const formState = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+let formData = { email: "", message: "" };
+
+const localState = JSON.parse(localStorage.getItem(formState));
+if (localState) {
+  formData = localState;
+  document.querySelector('[name="email"]').value = formData.email;
+  document.querySelector('[name="message"]').value = formData.message;
+}
+
+form.addEventListener('input', onInputSaveToLocalStorage);
+
+form.addEventListener('submit', onSubmitForm);
+
+
+function onInputSaveToLocalStorage(event) {
+  const { name, value } = event.target;
+  formData[name] = value.trim(); 
+
+  localStorage.setItem(formState, JSON.stringify(formData));
+}
+
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  if (formData.email === "" || formData.message === "") {
+    alert("Please fill in all fields");
+    return;
+  }
+
+  console.log('Form submitted with data:', formData);
+
+  localStorage.removeItem(formState);
+  formData = { email: "", message: "" };
+
+  form.reset();
+}
